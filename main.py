5e1c946deb78533754b2f3fa4cfe1479e70a5cc7
Bhavani AM <58398927+BhavaniAM@users.noreply.github.com>
2024-03-29 20:19:36 +0530

    Fix bug

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import pickle
 from predict_class import predict_class
 from compute_metric_score import compute_metrics
 
-from google.colab import drive
-drive.mount('/content/drive')
+# from google.colab import drive
+# drive.mount('/content/drive')
 
 X = pd.read_csv('/content/drive/MyDrive/NNDL_Project/X_test_sat4.csv') 
 Y = pd.read_csv('/content/drive/MyDrive/NNDL_Project/y_test_sat4.csv')
@@ -29,7 +29,7 @@ img_no = random.randint(0, 79999)
 imshow(np.squeeze(x_train[img_no, :, :, 0:3]).astype(float)) 
 plt.show()
 
-model = sat_class_model()
+model = sat_class_model(num_classes = 4)
 model_pred = model.fit(x_train, y_train, batch_size = 64, epochs = 20, verbose = 1, validation_split = 0.20)
 
 file_name = 'sat_cls_model.sav'
